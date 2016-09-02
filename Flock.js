d2b8c00b2158f8b75b262c724f3e1850dcd3b0db
Benjamin M. Brown <benjaminbb@gmail.com>
2016-09-02 18:59:49 -0500

    Enable 2d array creation for grid

--- Flock.js
+++ Flock.js
@@ -1,6 +1,21 @@
 var Flock = function(vehicles) {
    
 	this.vehicleFlock = [];
+	this.resolution = 10;
+	this.cols= width/this.resolution;
+	this.rows = height/this.resolution;
+
+    this.create2DArray = function(n) {
+        var array = [];
+        for (var i = 0; i < n; i++) {
+            array[i] = [];
+        }
+        return array;
+    }
+
+    this.grid = this.create2DArray(this.cols);
+
+    console.log(this.grid);
 
 	this.run = function(){
 		for(var i = 0; i<this.vehicleFlock.length; i++){
@@ -14,8 +29,10 @@ var Flock = function(vehicles) {
 	}
 
 	this.addVehicle = function(vehicle){
-		console.log('adding vehicle');
+		
 		this.vehicleFlock.push(vehicle);
 	}
+
+
 
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,26 @@
 var vehicles = [];
 var flock;
-var predators =[];
-var debug ;
+var predators = [];
+var debug;
+var scale = 10;
+var cols, rows;
 
 function setup() {
 
     createCanvas(1920, 1080);
     setFrameRate(50);
+
+    cols = width/scale;
+    rows = height/scale;
+    grid = create2DArray(cols,rows);
+
+    console.log("GRID",grid);
     flock = new Flock();
     predators = new Flock();
 
-    var type= 1;
+    var type = 1;
     for (var i = 0; i < 30; i++) {
-        var v = new Vehicle(width/2, height/2, type);
+        var v = new Vehicle(width / 2, height / 2, type);
         flock.addVehicle(v);
     }
 }
@@ -23,16 +31,21 @@ function draw() {
     predators.run();
 }
 
-function mouseDragged(){
-    flock.addVehicle(new Vehicle(mouseX, mouseY,1));
+function mouseDragged() {
+    flock.addVehicle(new Vehicle(mouseX, mouseY, 1));
 }
 
 function keyPressed() {
-
     for (var i = 0; i < key; i++) {
-        predators.addVehicle(new Vehicle(width/2, height/2));
+        predators.addVehicle(new Vehicle(width / 2, height / 2));
     }
+}
 
- 
+function create2DArray(cols,rows) {
+    var array = [];
+    for (var i = 0; i < cols; i++) {
+        array[i] = [];
+    }
+    return array;
 
 }
